498edc2f026ec175e8cdf10655a0426ae55b4993
Deokgyu IM <imdg3530@gmail.com>
2025-02-17 17:26:11 +0900

    250217

---
+++ 1244_스위치.py
@@ -0,0 +1,17 @@
+N = int(input()) # 스위치 개수
+arr = list(map(int, input().split())) #배열
+st = int(input()) #학생 수
+
+for tc in range(st):
+    s, number = map(int, input().split()) #성별, 받은 수
+
+    if s == 1:
+        if 
+
+
+
+
+'''
+★☆김은찬 왔다감★☆
+'''
+
