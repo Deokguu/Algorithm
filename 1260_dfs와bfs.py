a0ccd43df6ca323f328f3c01f9ca56083a17f3e8
Deokgyu IM <imdg3530@gmail.com>
2025-02-20 17:52:05 +0900

    250220

--- 1260_dfs와bfs.py
+++ 1260_dfs와bfs.py
@@ -15,6 +15,23 @@ def bfs(s):
                 visited[w] = 1
 
     return tracking
+def dfs(s):
+    tracking = []
+    visited = [0 for _ in range(V+1)]
+    stack = []
+    stack.append(s)
+    visited[s] = 1
+
+    while stack:
+        t = stack.pop()
+        tracking.append(t)
+
+        for w in sorted(G[t], reverse = True):
+            if visited[w] == 0:
+                stack.append(w)
+                visited[w] = 1
+
+    return tracking
         
         
 V, E, start = map(int, input().split())
@@ -23,4 +40,5 @@ for _ in range(E):
     v1, v2 = map(int, input().split())
     G[v1].append(v2)
     G[v2].append(v1)
+print(*dfs(start))
 print(*bfs(start))
